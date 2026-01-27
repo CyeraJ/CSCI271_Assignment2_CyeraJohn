824d96a92993a8653751b3213be7aff860a10069
Cyera John <johnc2@winthrop.edu>
2026-01-27 14:40:02 -0500

    Starting with documentation

---
+++ CSCI271_Assignment2_CyeraJohn.java
@@ -0,0 +1,48 @@
+/*************************************************************************
+* Assignment 2 for CSCI 271-001 Spring 2026
+*
+* Author: Cyera John
+* OS: Ubuntu Debian 24.04.3
+* Compiler: javac 21.0.9
+* Date: January 27, 2026
+*
+* Purpose
+*
+*************************************************************************/
+/*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
+*
+* Cyera John
+********************************************************************/
+import java.util.Scanner; //obtained from course notes
+
+public class CSCI271_Assignment2_CyeraJohn { /* obtained from course notes */
+/*****************************<main>****************************
+* Description: the main function of the code takes in arguements.
+*
+* Parameters: arguements
+*
+* Pre: class has to be made before anything
+*
+* Post: main code will have finished running as long as bracket is closed.
+*
+* Returns: nothing it is void.
+*
+* Called by: n/a
+* Calls: n/a
+************************************************************************/
+	public static void main(String[] args) { //obtained from course notes
+		Scanner input = new Scanner(System.in);
+	}
+}
